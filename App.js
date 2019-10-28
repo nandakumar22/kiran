eb76787419b5a3d2eb31644680e8ef1f5a4afbbb
kiran sai <53635996+kiran725@users.noreply.github.com>
2019-10-28 14:33:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,66 +6,78 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      password: '',
-      email: ''
+    password: '',
+    email: ''
     }
   }
 
   validate() {
     //include your validation inside if condition
-    if (this.state.password == "123$" && this.state.email == 'doogadmin.com') {
-     alert('Login success');
+    if (this.state.password == '' && this.state.email == '') {
+      alert('please enter email and password')
+    }
+
+    else if (this.state.email != "Doogadmin@.com") {
+      alert('incorrect email please try again');
+    }
+
+    else if (this.state.password != "123$") {
+      alert('incorrect password please try again');
+    }
+
+    else if (this.state.email == "Doogadmin@.com" && this.state.password != "123$") {
+      alert('incorrect details in password');
+    }
+
+    else if (this.state.password == "123$" && this.state.email != "Doogadmin@.com") {
+      alert('incorrect details in email');
+    }
+
+     else  {
+      alert('Login success');
       // navigate to next screen
     }
 
-    else if (this.state.email !== "doogadmin.com") {
-     alert('incorrect email');
-    }
-    
-    else if (this.state.password !== "123$") {
-     alert('incorrect password');
-    }
-
-    alert("Sorry Entered Value Dose not Exist.")
-  
   }
 
   render() {
     return (
-      <View style={{ justifyContent: 'center', backgroundColor: '#ddd', flex: 1 }}>
 
-        <Label>Enter Email</Label>
-        <Item inlineLabel>
 
+      <View style={{  padding: 40,backgroundColor: 'white', flex: 1 }}>
+
+       
+        <Item floatingLabel style={{ borderColor: 'black',
+                borderWidth:8, backgroundColor: 'transparent',}}>
+           <Label>Enter Email</Label>
           <Input
-            style={{ width: '90%', backgroundColor: 'white' }}
-            returnKeyType="go"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoFocus={true}
             onChangeText={(email) => this.setState({ email })} />
         </Item>
 
-        <Label>Enter Password</Label>
-        <Item inlineLabel>
 
+
+
+        <Item floatingLabel style={{ borderColor: 'black',
+                borderWidth:8, backgroundColor: 'transparent',marginTop:20}}>
+           <Label>Enter password</Label>
           <Input
-            style={{ width: '60%', backgroundColor: 'white' }}
-            returnKeyType="go"
-            secureTextEntry
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoFocus={true}
+              secureTextEntry
+              autoCapitalize="none"
+               autoCorrect={false}
+              autoFocus={true}
+
             onChangeText={(password) => this.setState({ password })} />
         </Item>
 
 
         <TouchableOpacity onPress={() => this.validate()}
-          style={{ padding: 15, marginTop: 20, width: 250, justifyContent: 'center', alignSelf: 'center', backgroundColor: 'rgb(102, 192, 231)' }}>
+          style={{ padding: 15, width: 250,marginTop:20, justifyContent: 'center', alignSelf: 'center', backgroundColor: 'rgb(102, 192, 231)' }}>
           <Text style={{ textAlign: 'center' }}>Next</Text>
         </TouchableOpacity>
 
       </View>
+
+    
     );
   }
 }
